f8f80e6e8ee9afb64a765d08a996a7222e829b2a
Lloyd Cooper <cooperc913@gmail.com>
2023-08-18 13:36:57 -0600

    clase Equipo con toString

--- src/Examen1P2_LloydCooper/Equipo.java
+++ src/Examen1P2_LloydCooper/Equipo.java
@@ -1,8 +1,57 @@
 package Examen1P2_LloydCooper;
 
+import java.util.ArrayList;
+
 public class Equipo {
     private String nombreEquipo;
     private String pais;
     private int ratingEquipo;
-    
+    private ArrayList<Jugador> plantillas = new ArrayList();
+
+    public Equipo(String nombreEquipo, String pais, int ratingEquipo) {
+        this.nombreEquipo = nombreEquipo;
+        this.pais = pais;
+        this.ratingEquipo = ratingEquipo;
+    }
+
+    public String getNombreEquipo() {
+        return nombreEquipo;
+    }
+
+    public void setNombreEquipo(String nombreEquipo) {
+        this.nombreEquipo = nombreEquipo;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public int getRatingEquipo() {
+        return ratingEquipo;
+    }
+
+    public void setRatingEquipo(int ratingEquipo) {
+        this.ratingEquipo = ratingEquipo;
+    }
+
+    public ArrayList<Jugador> getPlantillas() {
+        return plantillas;
+    }
+
+    public void setPlantillas(ArrayList<Jugador> plantillas) {
+        this.plantillas = plantillas;
+    }
+
+    @Override
+    public String toString() {
+        return "Equipo:\n" +
+               "Nombre del Equipo: " + nombreEquipo + "\n" +
+               "País: " + pais + "\n" +
+               "Rating del Equipo: " + ratingEquipo + "\n" +
+               "Plantillas: " + plantillas;
+    }
 }
